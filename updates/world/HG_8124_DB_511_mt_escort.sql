91086a1ee165693c18435cdc88fae5b689e3fa9e
Siarkowy <siarkowy@siarkowy.net>
2014-07-31 03:16:49 +0200

    Whoops, use ASCII in previous commit

--- updates/world/HG_8124_DB_511_mt_escort.sql
+++ updates/world/HG_8124_DB_511_mt_escort.sql
@@ -1,7 +1,7 @@
 -- Mana Tombs/Escort: Text and emote cleanup
 
 update script_texts set content_default = 'Hrm, now where is the next collector?' where entry = -1900241;
-update script_texts set content_default = 'He was right, you know. I''ll have to take that tag-line for my own… It''s not like he''ll have a use for it anymore!' where entry = -1900251;
+update script_texts set content_default = 'He was right, you know. I''ll have to take that tag-line for my own... It''s not like he''ll have a use for it anymore!' where entry = -1900251;
 
 delete from script_texts where entry in (-1966601, -1966602);
 insert into script_texts values
@@ -14,5 +14,5 @@ insert into creature_ai_scripts values
 (1966602,19666,4,0,100,6,0,0,0,0,1,-816,0,0,0,0,0,0,0,0,0,0,'Shadow Lord Xiraxis - Say on Aggro');
 
 delete from creature_ai_texts where entry = -815;
-insert into creature_ai_texts values (-815, 'Bravo! Bravo! Good show… I couldn''t convince you to work for me, could I? No, I suppose the needless slaughter of my employees might negatively impact your employment application.', null, null, null, null, null, null, null, null, 0, 1, 0, 0, 19666);
+insert into creature_ai_texts values (-815, 'Bravo! Bravo! Good show... I couldn''t convince you to work for me, could I? No, I suppose the needless slaughter of my employees might negatively impact your employment application.', null, null, null, null, null, null, null, null, 0, 1, 0, 0, 19666);
 update creature_ai_texts set content_default = 'Never underestimate the other ethereal''s greed!' where entry = -816;
