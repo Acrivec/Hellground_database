174d6850f9799d51f0a296a4be06a39e68eb495a
Siarkowy <siarkowy@siarkowy.net>
2015-04-02 17:14:18 +0200

    Fix previous SQLs

--- updates/world/HG_8596_guard_fengus.sql
+++ updates/world/HG_8596_guard_fengus.sql
@@ -1,3 +1,3 @@
 -- Dire Maul: Add Guard Fengus to instance
-INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`) VALUES
-(84379, 14321,  429,  0,  0,  1,  1,  0,  1,  392.82, 257.5,  11.62,  3.28, 1000000,  0,  0,  23688,  0,  0,  0,  0,  0,  0);
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `DeathState`, `MovementType`) VALUES
+(NULL,    14321,  429,    1,  0,  0,  384.548,    255.978,    11.4395,    4.70707,    300,    0,  0,  23688,  0,  0,  0);
