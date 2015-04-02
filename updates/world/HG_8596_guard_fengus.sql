66c4a4783c5343f6c1409a7b683f130d4141e695
Siarkowy <siarkowy@siarkowy.net>
2015-04-02 02:05:44 +0200

    Dire Maul: Add Guard Fengus to instance

---
+++ updates/world/HG_8596_guard_fengus.sql
@@ -0,0 +1,3 @@
+-- Dire Maul: Add Guard Fengus to instance
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`) VALUES
+(84379, 14321,  429,  0,  0,  1,  1,  0,  1,  392.82, 257.5,  11.62,  3.28, 1000000,  0,  0,  23688,  0,  0,  0,  0,  0,  0);
