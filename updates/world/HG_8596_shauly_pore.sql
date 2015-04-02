aaa6a33613145cf4e2449c9ed0024afd75e2e05d
Siarkowy <siarkowy@siarkowy.net>
2015-04-02 02:12:19 +0200

    Netherstorm: Add Shauly Pore to the map

---
+++ updates/world/HG_8596_shauly_pore.sql
@@ -0,0 +1,3 @@
+-- Netherstorm: Add Shauly Pore to the map
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`) VALUES
+(73662, 20921,  530,  0,  0,  1,  1,  0,  0,  3373.58,  2892.23,  144.049,  4.32842,  300,  0,  0,  5409, 3080, 0,  0,  0,  0,  0);
