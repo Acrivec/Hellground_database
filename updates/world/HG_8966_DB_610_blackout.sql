279449834b8194e4cecaf9e80f5927b9cd1f5eff
Siarkowy <siarkowy@siarkowy.net>
2015-08-08 15:05:53 +0200

    Semicolon

--- updates/world/HG_8966_DB_610_blackout.sql
+++ updates/world/HG_8966_DB_610_blackout.sql
@@ -1,1 +1,1 @@
-UPDATE spell_proc_event SET spellfamilymask = 42508288, schoolMask = 32 WHERE entry = 46025
+UPDATE spell_proc_event SET spellfamilymask = 42508288, schoolMask = 32 WHERE entry = 46025;
