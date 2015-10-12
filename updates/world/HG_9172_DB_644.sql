38ada8b19ef60b327a7967238a8a26393799dce4
Elyrion <elyrion.hg@gmail.com>
2015-10-12 15:51:43 +0200

    small fail :P

--- updates/world/HG_9172_DB_644.sql
+++ updates/world/HG_9172_DB_644.sql
@@ -1,1 +1,1 @@
-UPDATE `creature_template` SET `minhealth` = '1500' , `maxhealth` = '1500' , `minmana` = '16,155' , `maxmana` = '16,155' WHERE `entry` = '25598'; 
+UPDATE `creature_template` SET `minhealth` = '1500' , `maxhealth` = '1500' , `minmana` = '16155' , `maxmana` = '16155' WHERE `entry` = '25598'; 
